46568b4702a9a4c41750b941d2f3a8b96ba97fe3
Mateus Filipe <mateusfilipe557@gmail.com>
2018-06-30 23:09:25 -0300

    .

--- AEDS-I/MateusFilipe_Roteiro-07/MateusFilipe_ex21.cpp
+++ AEDS-I/MateusFilipe_Roteiro-07/MateusFilipe_ex21.cpp
@@ -23,23 +23,26 @@ const int C = 3;
 
 using namespace std;
 
-int ack(int n , int m);
+int ack(int m , int n){
+    int x;
+    if(m == 0){
+        return n+1;
+    }
+    if(m > 0 && n == 0){
+        return ack(m-1,1);
+    }
+    if(m > 0 && n > 0){
+        x=ack(m,n-1);
+        return ack(m-1,x);
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL , "Portuguese");
     int n, m;
-    cin>>n>>m;
+    cin>>m>>n;
     cout<<ack(m,n)<<endl;
     system("PAUSE");
     return 0;
-}
-int ack(int n , int m){
-    if(m == 0){
-        return n+1;
-    }else if(m > 0 && n == 0){
-        return ack(m-1,1);
-    }else if(m > 0 && n > 0){
-        return ack(m-1,ack(m,n-1));
-    }
 }
