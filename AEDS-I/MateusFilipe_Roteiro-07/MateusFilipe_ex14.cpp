34f27b21ee46a7e421ee4658dc5361973a19988d
Mateus Filipe <mateusfilipe557@gmail.com>
2018-07-03 16:43:54 -0300

    .

--- AEDS-I/MateusFilipe_Roteiro-07/MateusFilipe_ex14.cpp
+++ AEDS-I/MateusFilipe_Roteiro-07/MateusFilipe_ex14.cpp
@@ -1,19 +1,7 @@
  //Mateus Filipe De Lima Souza - 1º Período - BSI
 /*
     Exemplo de Teste{
-        A(n):
-        2
-        B(m):
-        2
-        Preencha o vetor:
-        1 3
 
-        Preencha o vetor:
-        2 4
-
-        A: Vetor Original: 1 | 3 |
-        B: Vetor Original: 2 | 4 |
-        C: Vetor Original: 1 | 2 | 3 | 4 |
     }
 */
 #include <iostream>
@@ -26,7 +14,7 @@
 
 //Vetor:
 
-const int TAM = 0;
+const int TAM = 100;
 
 //Matriz:
 
@@ -35,70 +23,107 @@ const int C = 3;
 
 using namespace std;
 
-void preencheVetor(float vetor[100], float n);
-void imprimeVetor(float vetor[100], float n);
-void intercalaVetor(float vetorA[100], float vetorB[100], float vetorC[100], float nm);
-void semRepeticao(float vetorA[100], float vetorB[100], float vetorC[100], float nm);
+void preencheVetor(int v[TAM], int a){
+    for(int i = 0 ; i < a ; i++){
+        cin>>v[i];
+    }
+}
+void imprimeVetor(int v[TAM], int a){
+    for(int i = 0 ; i < a ; i++){
+        cout<<v[i]<<" | ";
+    }
+    cout<<endl;
+}
+void intercalaVetor(int A[TAM], int B[TAM], int C[TAM], int nA, int nB, int nC);
+
+void comuns(int A[TAM], int B[TAM], int C[TAM], int nA, int nB, int &nC);
+
+void distintos(int A[TAM], int B[TAM], int C[TAM], int nA, int nB, int &nC);
+
+
 
 int main()
 {
     setlocale(LC_ALL , "Portuguese");
-    float vetorA[100], vetorB[100], vetorC[100], vetorC1[100], n, m;
+    int A[TAM], B[TAM], C[TAM], nA, nB, nC=0, D[TAM], nD=0, sR[TAM];
 
     cout<<"A(n): "<<endl;
-    cin>>n;
+    cin>>nA;
     cout<<"B(m): "<<endl;
-    cin>>m;
-
-    float nm = n+m;
-    preencheVetor(vetorA, n);
-    preencheVetor(vetorB, m);
+    cin>>nB;
+    nC=nA+nB;
+    preencheVetor(A, nA);
+    preencheVetor(B, nB);
+    intercalaVetor( A, B, C, nA, nB,nC);
     cout<<"A: ";
-    imprimeVetor(vetorA, n);
+    imprimeVetor(A, nA);
     cout<<"B: ";
-    imprimeVetor(vetorB, m);
-    intercalaVetor(vetorA, vetorB, vetorC, nm);
-    cout<<"C: ";
-    semRepeticao(vetorA, vetorB, vetorC1, nm);
-    imprimeVetor(vetorC, nm);
-    cout<<"C1: ";
-    imprimeVetor(vetorC1, nm);
+    imprimeVetor(B, nB);
+    cout<<"Intercalado: ";
+    imprimeVetor(C, nC);
+    cout<<endl;
+    comuns(A, B, D, nA, nB, nD);
+    cout<<"Comuns: ";
+    imprimeVetor(D, nD);
+
+    distintos(A, B, sR, nA, nB, nD);
+    cout<<"Sem Repetir: ";
+    imprimeVetor(sR, nD);
 
     system("PAUSE");
     return 0;
 }
-void preencheVetor(float vetor[100], float n){
-    cout<<"Preencha o vetor:"<<endl;
-    for(int i = 0 ; i < n ; i++){
-        cin>>vetor[i];
-    }
-    cout<<endl;
-}
-void imprimeVetor(float vetor[100], float n){
-    cout<<"Vetor Original: ";
-    for(int i = 0 ; i < n ; i++){
-        cout<<vetor[i]<<" | ";
-    }
-    cout<<endl;
-}
-void intercalaVetor(float vetorA[100], float vetorB[100], float vetorC[100], float nm){
-    int a = 0 , b = 0;
-    for(int k = 0; k < nm ; k++){
-        if(k%2==0){
-            vetorC[k]=vetorA[a];
-            a++;
-        }
-        if(k%2!=0){
-            vetorC[k]=vetorB[b];
-            b++;
+void intercalaVetor(int A[TAM], int B[TAM], int C[TAM], int nA, int nB, int nC){
+    for(int iA=0, iB=0, iC=0; iC<nC; iA++, iB++){
+        if(iA<nA and iB<nB){
+            C[iC]=A[iA];
+            iC++;
+            C[iC]=B[iB];
+            iC++;
+        }else if(iA >= nA){
+            C[iC]=B[iB];
+            iC++;
+        }else if(iB>=nB){
+            C[iC]=A[iA];
+            iC++;
         }
     }
 }
-void semRepeticao(float vetorA[100], float vetorB[100], float vetorC1[100], float nm){
-    for(int i = 0; i < nm ; i++){
-        if(vetorA[i] != vetorB[i])
-            vetorC1[i]=vetorA[i];
-        if(vetorB[i]!= vetorA[i])
-            vetorC1[i]=vetorB[i];
+void comuns(int A[TAM], int B[TAM], int D[TAM], int nA, int nB, int &nD){
+
+    for(int i = 0 ; i < nA ; i++){
+        for(int j = 0 ; j < nB ; j++){
+            if(A[i]==B[j]){
+                if(nD==0){
+                    D[nD]=A[i];
+                    nD++;
+                }else{
+                    bool achou = false;
+                    for(int z=0; z<nD; z++){
+                        if(A[i]==D[z]){
+                            achou = true;
+                        }
+                    }
+                    if(achou==false){
+                        D[nD]=A[i];
+                        nD++;
+                    }
+                }
+            }
+        }
+    }
+}
+void distintos(int A[TAM], int B[TAM], int D[TAM], int nA, int nB, int &nD){
+    for(int i = 0 ; i < nA ; i++){
+        for(int j = 0 ; j < nB; j++){
+            if(A[i]!=B[i]){
+                if(nD==0){
+                    D[nD]=A[i];
+                    nD++;
+                }else{
+
+                }
+            }
+        }
     }
 }
