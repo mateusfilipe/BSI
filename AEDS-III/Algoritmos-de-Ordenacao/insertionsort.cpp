8f8e4db5bc48704daa52ce201164890df79dbf15
Mateus Filipe <mateusfilipe557@gmail.com>
2019-03-30 15:28:57 -0300

    insertion sort - começando pelo penultimo

---
+++ AEDS-III/Algoritmos-de-Ordenacao/insertionsort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  int aux, n, j;
+//  cout<<"Tamanho: ";
+//  cin>>n;
+  n = 6;
+  cout<<"Vetor: "<<endl;
+  int *vet = new int[n];
+  for(int i = 0 ; i < n ; i++){
+    cin>>vet[i];
+  }
+  //Ordena:
+  for(int i = n-1 ; i > 0 ; i--){
+    aux = vet[i];
+    j = i+1;
+      while(j <= n && vet[j] < aux){
+        vet[j-1] = vet[j];
+        j++;
+      }
+      vet[j-1] = aux;
+  }
+  //
+  cout<<endl;
+  for(int i = 0 ; i < n ; i++){
+    cout<<vet[i]<<" ";
+  }
+}
