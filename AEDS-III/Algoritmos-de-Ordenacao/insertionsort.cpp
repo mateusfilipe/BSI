6a6c7fb89ef6b8219518dedeec41d4cc98be3bdd
Mateus Filipe <mateusfilipe557@gmail.com>
2019-04-02 18:32:36 -0300

    Insertion Sort - now with cont
    insertion sort with cont and +

--- AEDS-III/Algoritmos-de-Ordenacao/insertionsort.cpp
+++ AEDS-III/Algoritmos-de-Ordenacao/insertionsort.cpp
@@ -11,15 +11,18 @@ int main(){
   for(int i = 0 ; i < n ; i++){
     cin>>vet[i];
   }
+  int cont = 0;
   //Ordena:
   int aux;
   for(int i = n-1 ; i >= 0 ; i--){
+    cont++;
     aux = vet[i];
     j = i+1;
     cout<<"aux: "<<aux<<endl;
     cout<<"i: "<<i<<endl;
     cout<<"j: "<<j<<endl;
       while(j < n && vet[j] < aux){
+        cont++;
         vet[j-1] = vet[j];
         j++;
       }
@@ -30,4 +33,5 @@ int main(){
   for(int i = 0 ; i < n ; i++){
     cout<<vet[i]<<" ";
   }
+  cout<<cont<<endl;
 }
