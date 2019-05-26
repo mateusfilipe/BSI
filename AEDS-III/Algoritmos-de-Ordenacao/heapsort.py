f061c9a75c527d21f3b143a0d03b9cd0b1888148
Mateus Filipe <mateusfilipe557@gmail.com>
2019-05-26 19:44:28 -0300

    heapsort.py implementado

--- AEDS-III/Algoritmos-de-Ordenacao/heapsort.py
+++ AEDS-III/Algoritmos-de-Ordenacao/heapsort.py
@@ -1,11 +1,10 @@
 #Mateus Filipe
-#Algoritmo de ordenação Shellsort
+#Algoritmo de ordenação Heapsort
 
 def heapify(vetor, n, i):
     maior = i
     l = 2 * i + 1
-    r = 2 * 1 + 2
-
+    r = 2 * i + 2
     if l < n and vetor[i] < vetor[l]:
         maior = l
     if r < n and vetor[maior] < vetor[r]:
@@ -13,12 +12,15 @@ def heapify(vetor, n, i):
     if maior != i :
         vetor[i], vetor[maior] = vetor[maior],vetor[i]
         heapify(vetor,n, maior)
-        
-def heapSort(vetor):
+
+def heapsort(vetor):
     n = len(vetor)
     for i in range(n, -1, -1):
         heapify(vetor, n, i)
     for i in range(n-1, 0, -1):
+        vetor[i],vetor[0] = vetor[0],vetor[i]
         heapify(vetor, i, 0)
     print(vetor)
-heapSort([5 , 2 , 4 , 6 , 1 , 3])
+
+vetor = [5 , 2 , 4 , 6 , 1 , 3]
+heapsort(vetor)
