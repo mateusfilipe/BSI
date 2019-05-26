6b40b00fde70b55f3a73bd68a0be3e98fa5bcdae
Mateus Filipe <mateusfilipe557@gmail.com>
2019-05-26 19:24:52 -0300

    insertionsort.py implementado

---
+++ AEDS-III/Algoritmos-de-Ordenacao/insertionsort.py
@@ -0,0 +1,14 @@
+#Mateus Filipe
+#Algoritmo de ordenação Shellsort
+
+def ordenar(vetor):
+    n = len(vetor)
+    for i in range(n):
+        key = vetor[i]
+        j = i - 1
+        while j >= 0 and vetor[j] > key:
+            vetor[j+1] = vetor[j]
+            j -= 1
+        vetor[j+1] = key
+    print(vetor)
+ordenar([5 , 2 , 4 , 6 , 1 , 3])
