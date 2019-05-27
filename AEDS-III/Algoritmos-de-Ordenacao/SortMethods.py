29b695db5145e4263dde94c491cee9768e29f1f5
Mateus Filipe <mateusfilipe557@gmail.com>
2019-05-27 15:22:17 -0300

    Adicionando QuickSort junto aos outros métodos

--- AEDS-III/Algoritmos-de-Ordenacao/SortMethods.py
+++ AEDS-III/Algoritmos-de-Ordenacao/SortMethods.py
@@ -1,8 +1,7 @@
-'''
-O seguinte código contém diversos métodos de ordenação
-BubbleSort, InsertSort, SelectSort, ShellSort, QuickSort, HeapSort e Mer-
-geSort.
-'''
+'''O seguinte código contém diversos métodos de ordenação
+BubbleSort, InsertSort, SelectSort, ShellSort, QuickSort,
+HeapSort e MergeSort.'''
+
 def BubbleSort(vetor):
     auxiliar = 0
     troca = False
@@ -57,7 +56,47 @@ def ShellSort(vetor):
         gap //= 2
     #print(vetor)
 
-#def QuickSort():
+'''QuickSort: '''
+def quicksort(V):
+    quicksort2(V, 0, len(V) - 1)
+
+
+def quicksort2(V, menor, maior):
+    if menor < maior:
+        p = particao(V, menor, maior)
+        quicksort2(V, menor, p - 1)
+        quicksort2(V, p + 1, maior)
+
+
+def achapivo(V, menor, maior):
+    meio = (maior + menor) // 2
+    pivo = maior
+    if V[menor] < V[meio]:
+        pivo = meio
+
+    elif V[menor] < V[maior]:
+        pivo = menor
+
+    return pivo
+
+
+def particao(V, menor, maior):
+    indicepivo = achapivo(V, menor, maior)
+    valorpivo = V[indicepivo]
+    V[indicepivo], V[menor] = V[menor], V[indicepivo]
+    borda = menor
+
+    for i in range(menor, maior + 1):
+        if V[i] < valorpivo:
+            borda += 1
+            V[i], V[borda] = V[borda], V[i];
+    V[menor], V[borda] = V[borda], V[menor]
+
+    return borda
+
+'''Fim do QuickSort'''
+
+
 '''HeapSort: '''
 def heapify(vetor, n, i):
     maior = i
@@ -110,3 +149,5 @@ def MergeSort(vetor):
             vetor[k] = R[j]
             j+=1
             k+=1
+
+def main():
